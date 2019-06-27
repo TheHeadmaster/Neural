237acc2a6c9504573d48293072abe4e961b7a8e5
David Palmer <davidivpalmer@gmail.com>
2019-06-26 20:56:49 -0500

    Added SignalR dependency

---
+++ Neural/Abstraction/Element.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neural.Abstraction
+{
+    /// <summary>
+    /// Represents an html element, such as div, p, or text.
+    /// </summary>
+    public abstract class Element
+    {
+    }
+}
